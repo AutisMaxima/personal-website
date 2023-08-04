e352c156a7b06d8f93b935efa692daeab4d73116
Rafael Tauran <tauran.rafaelzone.rafael@gmail.com>
2023-08-04 14:33:41 +0200

    Added a custom hello div to the example page.

--- src/app/example/page.tsx
+++ src/app/example/page.tsx
@@ -1,5 +1,11 @@
 const Example = () => {
-    return (<p className="bg-primary">Hello World!</p>);
+    return (
+        <div style={{ position: 'absolute', top: 0, left: 0, pointerEvents: 'none', width: '100%', height: '100%' }}>
+            <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate3d(-50%,-50%,0)' }}>
+                <h1 style={{ margin: 0, padding: 0, fontSize: '15em', fontWeight: 500, letterSpacing: '-0.05em' }}>hello</h1>
+            </div>
+        </div>
+    );
 };
 
 export default Example;
