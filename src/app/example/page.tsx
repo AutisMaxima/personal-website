d7b30c79a668d899ff08934cb1aca985711f4bb8
Rafael Tauran <tauran.rafaelzone.rafael@gmail.com>
2023-08-05 17:19:16 +0200

    Modified the landing page

--- src/app/example/page.tsx
+++ src/app/example/page.tsx
@@ -1,13 +1,23 @@
 import TextReveal from "./text-reveal";
-import { Fragment } from "react";
+
 const Example = () => {
 
+    const backgroundImgArr = [
+        'https://daisyui.com/images/stock/photo-1507358522600-9f71eß620c44e.jpg',
+        '/IMG_6243.jpg'
+    ];
+
     return (
-            <div className="flex h-screen justify-center items-center">
-                <div className="text-center">
-                    <TextReveal text="Welcome Back!" />
+        <div className="hero min-h-screen" style={{ backgroundImage: `url(${backgroundImgArr[Math.floor(Math.random() * backgroundImgArr.length)]})` }}>
+            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content text-center text-peimary-content">
+                <div className="max-w-md">
+                    <TextReveal text="Hello There" definedClass="mb-5 text-5xl font-bold" />
+                    <TextReveal text="Weclome to my website. Come, take a seat and explore!" definedClass="mb-5 text-lg" />
+                    <button className="btn btn-primary">Get Started</button>
                 </div>
             </div>
+        </div>
     );
 };
 
--- src/app/example/text-reveal.tsx
+++ src/app/example/text-reveal.tsx
@@ -2,55 +2,26 @@
 
 import { motion } from "framer-motion";
 
-const TextReveal = ({ text }: { text: String }) => {
-
-    // Split text into and array of words
-    const characters = text.split("");
+const TextReveal = ({ text, definedClass = ""}: { text: string, definedClass: string }) => {
 
     // Sentence gives the instruction on the delay of the animation of each word
     const sentence = {
         hidden: { opacity: 0 },
         visible: (i = 1) => ({
             opacity: 1,
-            transition: { staggerChildren: 0.12, delayChildren: 0.04 * i },
+            transition: { staggerChildren: 0.12, delayChildren: 0.12 * i },
         }),
     };
 
-    // Letter does the main animation
-    const letter = {
-        visible: {
-            opacity: 1,
-            x: 0,
-            transition: {
-                type: "spring",
-                damping: 12,
-                stiffness: 50,
-            },
-        },
-        hidden: {
-            opacity: 0,
-            x: 20,
-            transition: {
-                type: "spring",
-                damping: 12,
-                stiffness: 50,
-            },
-        },
-    };
-
     return (
-        <motion.h1
-            className="m-0 p-0 text-6xl sm:text-8xl md:text-10xl lg:text-12xl bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary font-sans font-semibold tracking-tighter leading-none"
+        <motion.div
+            className={definedClass}
             variants={sentence}
             initial="hidden"
             animate="visible"
         >
-            {characters.map((char, index) => (
-                <motion.span key={char + "-" + index} variants={letter}>
-                    {char}
-                </motion.span>
-            ))}
-        </motion.h1>
+        {text}
+        </motion.div>
     );
 };
 
