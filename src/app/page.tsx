244af12e3feb0367bb80190286ef299f9747de0b
Rafael Tauran <tauran.rafaelzone.rafael@gmail.com>
2023-08-15 15:41:36 +0700

    Converted dashboard to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,92 @@
-import Image from 'next/image'
+import { Fragment } from "react";
+import FadeInWhenVisible from "@/components/animations/fadeInWhenVisible";
+
+import Carousel from "@/components/carousel/carousel";
+import Hero from "@/components/hero";
+import Image from "next/image";
+
+// Image array
+const imageArray = [
+    {
+        src: "/duomo-side.jpg",
+        alt: "Duomo Side",
+        width: 400,
+        height: 400,
+        href: "https://google.com/"
+    },
+    {
+        src: "/duomo-road.jpg",
+        alt: "Duomo Road",
+        width: 400,
+        height: 400,
+        href: "https://youtube.com/"
+    },
+    {
+        src: "/milan.jpg",
+        alt: "Milan City Centre",
+        width: 400,
+        height: 400,
+        href: "https://bing.com/"
+    }
+];
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff40] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+    return (
+        <Fragment>
+            <Hero flex="flex-col" imgurl="/studying-girl.webp">
+                <FadeInWhenVisible>
+                <h1 className="text-gray-200 text-4xl lg:text-8xl">Hello There!</h1>
+                <p className="text-center">Welcome back</p>
+                </FadeInWhenVisible>
+            </Hero>
+            <Hero
+                definedClass="px-1 md:px-3 lg:px-5 py-1 md:py-3 lg:py-5 bg-gradient-to-br from-pink-600 from-10% to-slate-950 to-90% h-[48rem]"
+            >
+                <Image
+                    alt="some picture"
+                    src="/milan.jpg"
+                    width={500}
+                    height={500}
+                    className="rounded-lg shadow-2xl"
+                />
+                <FadeInWhenVisible>
+                    <h1 className="text-5xl text-gray-200 font-bold text-right">About</h1>
+                    <p className="py-6 text-right text-gray-200">
+                        I am a passionate 4th year computer science student at the University of Warwick,
+                        exploring the realms of technology and innovation. By exploring, I mean trying out
+                        new stuff which I find interesting. As you can probably already tell, the first sentence
+                        was generated by ChatGPT.
+                    </p>
+                </FadeInWhenVisible>
+            </Hero>
+            <Hero
+                definedClass="px-1 md:px-3 lg:px-5 py-1 md:py-3 lg:py-5 bg-gradient-to-bl from-teal-500 from-10% to-slate-950 to-90% h-[48rem]"
+            >
+                <FadeInWhenVisible>
+                    <h1 className="text-5xl text-gray-200 font-bold">Content</h1>
+                    <p className="py-6 text-gray-200">
+                        I post my personal projects and notes here. 
+                        I experiment with different frontend and backend features here as well.
+                    </p>
+                </FadeInWhenVisible>
+                <Image
+                    alt="some picture"
+                    src="/milan.jpg"
+                    width={500}
+                    height={500}
+                    className="rounded-lg shadow-2xl"
+                />
+            </Hero>
+            <Hero
+                flex="flex-col"
+                definedClass="px-1 md:px-3 lg:px-5 py-1 md:py-3 lg:py-5 bg-gradient-to-br from-cyan-400 from-10% to-slate-950 to-90% h-[48rem]"
+            >
+                <FadeInWhenVisible>
+                    <h1 className="text-5xl text-gray-200 font-bold text-center">Where to begin?</h1>
+                    <p className="py-6 text-gray-200">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                </FadeInWhenVisible>
+                <Carousel isLinked={true} height="h-96" carouselItems={imageArray} />
+            </Hero>
+        </Fragment>
+    );
+};
