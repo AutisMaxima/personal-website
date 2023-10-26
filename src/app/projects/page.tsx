846710e77001e0ab3be02ef98f5c683d1e78d6be
Rafael Tauran <tauran.rafaelzone.rafael@gmail.com>
2023-10-26 16:21:33 +0100

    Finished created the projects website.

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -45,6 +45,47 @@ const Projects = () => {
 
             <div className="my-[2.5%]"><h3 className="text-xl md:text-2xl lg:text-3xl text-emphasis text-base-content">C++</h3></div>
             <div className="divider mb-[4%] text-base-content"></div>
+
+
+            {/** C++ Parser  */}
+            <div className="card card-normal min-h-fit bg-base-100 shadow-xl mb-[4%]">
+                <div className="card-body">
+                    <h2 className="card-title text-base-content text-base md:text-lg lg:text-xl">
+                        Parser for a subset of the C language
+                    </h2>
+
+                    <p className="text-base-content text-sm md:text-base lg:text-lg">LL1 Parser for a subset of the C language that is extendable to a full compiler in LLVM.</p>
+                    <div className="card-actions justify-end text-primary-content">
+                        <Link href="https://github.com/AutisMaxima/accent-recognition" className="btn btn-primary btn-sm md:btn-md lg:btn-lg">
+                            <span><CloudIcon className="w-6 h-6" /></span>
+                            <span>GitHub</span>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="my-[2.5%]"><h3 className="text-xl md:text-2xl lg:text-3xl text-emphasis text-base-content">Web Apps</h3></div>
+            <div className="divider mb-[4%] text-base-content"></div>
+
+
+            {/** Discord Bot  */}
+            <div className="card card-normal min-h-fit bg-base-100 shadow-xl mb-[4%]">
+                <div className="card-body">
+                    <h2 className="card-title text-base-content text-base md:text-lg lg:text-xl">
+                        Discord Bot
+                    </h2>
+
+                    <p className="text-base-content text-sm md:text-base lg:text-lg">A discord bot that insults your friends, displays weather of different cities, and finds comics.</p>
+                    <div className="card-actions justify-end text-primary-content">
+                        <Link href="https://github.com/AutisMaxima/accent-recognition" className="btn btn-primary btn-sm md:btn-md lg:btn-lg">
+                            <span><CloudIcon className="w-6 h-6" /></span>
+                            <span>GitHub</span>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
